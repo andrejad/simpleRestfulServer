7fd3e23ee093e28d451402ccfe94f9cfec3097eb
andrejad <andreja.dulovic@gmail.com>
2017-03-27 20:46:49 +0200

    Update and rename srv1.py to restServer.py
    Name changed. Cleanup code.

--- restServer.py
+++ restServer.py
@@ -1,4 +1,4 @@
-import dbApp
+import dbService
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api, reqparse
 
@@ -20,7 +20,7 @@ parser.add_argument('owner', required=True, help="Owner must be supplied!", loca
 parser.add_argument('priority', type=int, required=True, location = 'args') #not mandatory, default 1
 parser.add_argument('taskId', type=int, location = 'args') #not mandatory, default 1
 
-# For browser call http://000.000.000.000:5000/ this will display the list of REST methods exposed.
+# For browser call http://127.0.0.1:5000/ this will display the list of REST methods exposed.
 # For "new" and "update" functions there is an alternative with curl linux command and how to use it.
 @app.route('/')
 def index():
@@ -68,12 +68,7 @@ class UpdateRec(Resource):
         print ("UpdateRec(", json_data['taskId'], json_data['owner'], json_data['priority'], json_data['message'], ")")
         # TODO: validation
         d = dbApp.updateDataT(json_data['taskId'], json_data['owner'], json_data['priority'], json_data['message'])
-        return jsonify(d)        
-        # args = parser.parse_args()        
-        # print ("UpdateRec(", str(args['taskId']), args['owner'], args['priority'], args['message'], ")")
-        # # TODO: validation
-        # d = dbApp.updateDataT(args['taskId'], args['owner'], args['priority'], args['message'])
-        # return jsonify(d)        
+        return jsonify(d)                
 
 # Deletes a record in the table referenced as rowid.
 class DeleteRec(Resource):
