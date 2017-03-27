fee3910823fd63bec0f6592abcdd47cfbe314840
andrejad <andreja.dulovic@gmail.com>
2017-03-27 20:51:27 +0200

    Update and rename dbApp.py to dbService.py
    Change name. Code cleaned up. Comments inserted.

--- dbService.py
+++ dbService.py
@@ -11,9 +11,12 @@ dbName = "datastore"
 # message(text)
 createDataT = "CREATE TABLE IF NOT EXISTS dataT (timestamp text DEFAULT CURRENT_TIMESTAMP, owner text, priority int, message text)"
 
+# dafault data to be loaded if table doesn't exist.
 defData = [["ana", 1, "note no.1"], ["bob", 1, "note #2"], ["carl", 2, "note #3"], ["dave", 1, "note #4"]]
 
+# creation of database (if doesn't exist), table, and inserting initial data.
 def initTableDataT():
+    
     print("initTableDataT()")
     # if not exist, create table
     con = sqlite3.connect(dbName)
@@ -30,7 +33,9 @@ def initTableDataT():
     con.close()
     print ("Number of rows in dataT table:", res[0])
 
+# fetching all data from the table (limited amount of rows)
 def getAllDataT(lim):
+    
     print("getAllDataT("+str(lim)+")")
     if lim > 100:
         print("getAllDataT absolute limit 100 rows")
@@ -53,7 +58,9 @@ def getAllDataT(lim):
     con.close()
     return d
 
+# fetching all records from the table, by owner
 def getAllByOwner(owner, lim):
+    
     if lim > 100:
         print("getAllDataT absolute limit 100 rows")
         lim = 100
@@ -73,7 +80,9 @@ def getAllByOwner(owner, lim):
     con.close()
     return d
 
+# inserting new record
 def putNewTask(owner, priority, message):
+    
     print ("db put new taks")
     # TODO: validation parameters
     con = sqlite3.connect(dbName)
@@ -83,6 +92,7 @@ def putNewTask(owner, priority, message):
     con.close()
     return {'status': 'ok'}
 
+# updateing a record, identified by rowid (primary key)
 def updateDataT(rowid, owner, priority, message):
     con = sqlite3.connect(dbName)
     cur = con.cursor()
@@ -92,7 +102,9 @@ def updateDataT(rowid, owner, priority, message):
     con.close()
     return {'status': 'ok'}
 
+# deleting a record, identified by rowid (primary key)
 def deleteTask(rowid):
+    
     con = sqlite3.connect(dbName)
     cur = con.cursor()
     cur.execute("delete from dataT where rowid=?", (rowid,))
@@ -100,7 +112,9 @@ def deleteTask(rowid):
     con.close()
     return {'status': 'ok'}
 
+# fetching a record based on rowid (PK)
 def getById(rowid):
+    
     con = sqlite3.connect(dbName)
     cur = con.cursor()
     cur.execute("select rowid, * from dataT where rowid=?", (rowid,))
@@ -109,7 +123,9 @@ def getById(rowid):
     con.close()
     return d
 
+# internal method for transforming db data to a list (dictionary)
 def rowsToListDict(rows):
+    
     retList = []
     for row in rows:
         dict = {
@@ -123,5 +139,8 @@ def rowsToListDict(rows):
     #print("retList", retList)
     return retList
 
+# create table if not already created
 initTableDataT()
+
+# read from table, for testing purposes.
 getAllDataT(3)
